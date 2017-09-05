20a62f6c0fc916593611fa95394148dd7df490aa
Gustavo Nunes <gm.nunes92@gmail.com>
2017-09-05 10:32:57 -0300

    Alterações de clientes

---
+++ POO-2017.1-Git/src/SistemaMusical/Bateria.java
@@ -0,0 +1,45 @@
+package SistemaMusical;
+
+public class Bateria implements Instrumento{
+
+    @Override
+    public String getMarca() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNumeroSerie() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getValor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getQtd() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setMarca(String marca) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setNumeroSerie(String numero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setValor(double valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setQtd(int qtd) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- POO-2017.1-Git/src/SistemaMusical/Instrumento.java
+++ POO-2017.1-Git/src/SistemaMusical/Instrumento.java
@@ -1,5 +1,13 @@
 package SistemaMusical;
 
 public interface Instrumento {
+    public String getMarca();
+    public String getNumeroSerie();
+    public double getValor();
+    public int getQtd();
     
+    public void setMarca(String marca);
+    public void setNumeroSerie(String numero);
+    public void setValor(double valor);
+    public void setQtd(int qtd);
 }
